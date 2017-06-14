026fc0b652aece806a606511504bcb76355ef4c1
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-06-14 14:51:04 +0530

    Update server.js to run after compilation

--- src/startServer.js
+++ src/startServer.js
@@ -1,2 +1,20 @@
-import { startServer } from "./server";
-startServer();
+import _ from "lodash";
+import express from "express";
+import http from "http";
+import Config from "config";
+
+const app = express();
+const server = http.createServer(app);
+const serverPort = _.get(Config, "server.port", 3000);
+
+// Include server routes as a middleware
+app.use(function(req, res, next) {
+  require("./server")(req, res, next);
+});
+
+server.listen(serverPort, "localhost", function(err) {
+  if (err) throw err;
+  const addr = server.address();
+  // eslint-disable-next-line
+  console.log("Listening at http://%s:%d", addr.address, addr.port);
+});
