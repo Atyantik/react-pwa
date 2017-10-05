799c43c95dffacab2e51d73fc410dda70254810a
Tirth Bodawala <tirthbodawala@atyantik.com>
2017-10-06 01:16:22 +0530

    Remove redirect reference from import

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 import classNames from "classnames";
 import  { Link } from "react-router-dom";
 import { renderSubRoutes } from "../../../core/utils/renderer";
