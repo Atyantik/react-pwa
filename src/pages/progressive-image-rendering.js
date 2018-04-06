fe7f3194796c1540241384f5946cc8f0cbe463b5
Jordan Zhang <jordanzhang@gmail.com>
2018-04-06 15:59:23 +0900

    Fixed spelling of Progressive.

--- src/pages/progressive-image-rendering.js
+++ src/pages/progressive-image-rendering.js
@@ -1,12 +1,12 @@
-import ProgresiveImage from "../app/components/progressive-image";
+import ProgressiveImage from "../app/components/progressive-image";
 import DefaultLayout from "../app/components/layout";
 
 const routes = [
   {
     path: "/progressive-image-rendering",
     exact: true,
-    component: ProgresiveImage,
-    layout: DefaultLayout,
+    component: ProgressiveImage,
+    layout: DefaultLayout
   }
 ];
 export default routes;
