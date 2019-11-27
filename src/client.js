db52372c845330f21f5b3cc87d0ce5fbd6d3f16e
Mayank Patel <patelmayankce@gmail.com>
2019-11-27 18:45:27 +0530

    comment util.scss from client.js to avoid error on build

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 // Bulma CSS for light weight CSS. One can any css framework
 import 'bulma/css/bulma.min.css';
-import './resources/css/util.scss';
+// import './resources/css/util.scss';
 import './resources/css/global.css';
 
 export default class Client {
