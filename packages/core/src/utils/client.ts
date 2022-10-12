1274294547711a4ace33d41128e632b1874e257a
Tirth Bodawala <tirthbodawala@atyantik.com>
2022-10-12 16:07:52 +0530

    Add client and cookie util to make code more readable

---
+++ packages/core/src/utils/client.ts
@@ -0,0 +1,59 @@
+import { RoutesArgs } from '../index.js';
+
+const scopedCache = new WeakMap();
+
+const setScopedVar = (key: string, value: any) => {
+  const scopedVals = scopedCache.get(window) ?? {};
+  scopedCache.set(window, {
+    ...scopedVals,
+    [key]: value,
+  });
+};
+
+const hasScopedVar = (key: string) => !!(scopedCache.get(window)?.[key] ?? false);
+
+const getScopedVar = <T extends any>(
+  key: string,
+  defaultValue?: T,
+) => scopedCache.get(window)?.[key] ?? defaultValue ?? null;
+
+const getScoped = async (
+  key: string,
+  cb: (() => any) | (() => Promise<any>),
+) => {
+  if (hasScopedVar(key)) {
+    return getScopedVar(key);
+  }
+  const computedScopeCB = await cb();
+  setScopedVar(key, computedScopeCB);
+  return computedScopeCB;
+};
+
+const { userAgent } = window.navigator;
+export const requestArgs: RoutesArgs = {
+  getLocation: () => new URL(window.location.href),
+  browserDetect: async () => {
+    const { parse } = await import('bowser');
+    try {
+      return parse(userAgent);
+    } catch {
+      // Cannot parse useragent
+    }
+    return {
+      browser: { name: '', version: '' },
+      os: { name: '', version: '', versionName: '' },
+      platform: { type: '' },
+      engine: { name: '', version: '' },
+    };
+  },
+  userAgent,
+  isbot: async () => {
+    const { default: isBot } = await import('isbot');
+    isBot.exclude(['chrome-lighthouse']);
+    return isBot(userAgent);
+  },
+  getScoped,
+  addToHeadPreStyles: () => {
+    // when called on server side do nothing
+  },
+};
