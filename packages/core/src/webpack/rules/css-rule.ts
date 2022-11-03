613580140357144d052af492c071ec441bc9d6db
Tirth Bodawala <tirthbodawala@atyantik.com>
2022-11-03 20:11:00 +0530

    Make public path auto to resolve appropriate css relative paths

--- packages/core/src/webpack/rules/css-rule.ts
+++ packages/core/src/webpack/rules/css-rule.ts
@@ -19,7 +19,7 @@ export const getCssRule = (
     options.hotReload && { loader: 'style-loader' },
     options.hotReload || {
       loader: MiniCssExtractPlugin.loader,
-      options: { emit: options.emit },
+      options: { emit: options.emit, publicPath: 'auto' },
     },
     {
       loader: 'css-loader',
